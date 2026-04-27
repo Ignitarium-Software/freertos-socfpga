b0b531dfff74c5067944ecef970d7e38aed388b8
muhammed-rafi-ign <muhammed.rafik@ignitarium.com>
2026-04-27 21:17:55 +0530

    Config: change tick rate to 500
    * The previous value 400 caused portTICK_PERIOD_MS precision loss
      due to TickType_t casting.The tick period is derived as
      1000/configTICK_RATE_HZ. Due to casting TickType_t to (uint64_t),
      the value was truncated to 2. Updated the calculation to avoid
      truncation and ensure accurate tick timing.
    
    Signed-off-by: muhammed.rafik <muhammed.rafik@ignitarium.com>

--- FreeRTOS/Demo/SOCFPGA/FreeRTOSConfig.h
+++ FreeRTOS/Demo/SOCFPGA/FreeRTOSConfig.h
@@ -46,7 +46,7 @@
 #define configUSE_PREEMPTION                    1
 #define configUSE_TICKLESS_IDLE                 0
 #define configCPU_CLOCK_HZ                      (SystemCoreClock)
-#define configTICK_RATE_HZ                      ((TickType_t)400)
+#define configTICK_RATE_HZ                      ((TickType_t)500)
 #define configMAX_PRIORITIES                    5
 #define configMINIMAL_STACK_SIZE                ((unsigned short)1024)
 #define configMAX_TASK_NAME_LEN                 20
